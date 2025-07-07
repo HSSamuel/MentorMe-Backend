cd57fb82115ebb82c49cbe24465336de87b2ba6b
Hunsa Semako S. <smkmayomisamuel@gmail.com>
2025-07-07 19:00:03 +0100

    Update

--- src/middleware/profileCheck.middleware.ts
+++ src/middleware/profileCheck.middleware.ts
@@ -1,7 +1,28 @@
-export const ensureProfileComplete = async (req, res, next) => {
-  const user = await prisma.user.findUnique({ where: { id: req.user.id } });
-  if (!user.name || !user.bio || !user.skills.length || !user.goals) {
-    return res.status(403).json({ message: "Complete profile required." });
+import { PrismaClient } from "@prisma/client";
+import { Request, Response, NextFunction } from "express";
+
+const prisma = new PrismaClient();
+
+export const ensureProfileComplete = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = await prisma.user.findUnique({ where: { id: req.user.id } });
+
+    if (
+      !user ||
+      !user.name ||
+      !user.bio ||
+      !user.skills.length ||
+      !user.goals
+    ) {
+      return res.status(403).json({ message: "Complete profile required." });
+    }
+    next();
+  } catch (error) {
+    console.error("Error checking profile completeness:", error);
+    return res.status(500).json({ message: "Internal server error." });
   }
-  next();
 };
